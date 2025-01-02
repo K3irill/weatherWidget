0430b37edacfaacb596a7e65ee95d61beb3edaa3
Kirill <razdor.2019@mail.ru>
2025-01-03 02:21:52 +0300

    feat: CurrentWeather

--- src/components/CurrentWeather/CurrentWeather.tsx
+++ src/components/CurrentWeather/CurrentWeather.tsx
@@ -4,10 +4,28 @@ import WeatherIcon from '../ui/WeatherIcon/WeatherIcon'
 const CurrentWeather = ({ weatherInfo }) => {
 	return (
 		<div className={styles.container}>
-			<WeatherIcon icon={weatherInfo.weather[0].icon} />
-			<h1 className={styles.temp}>{weatherInfo.main.temp.toFixed(0)}</h1>
-			<h2 className={styles.description}>{weatherInfo.weather[0].main}</h2>
-			<p></p>
+			<div className={styles.main}>
+				<WeatherIcon icon={weatherInfo.weather[0].icon} />
+				<h1 className={styles.temp}>{weatherInfo.main.temp.toFixed(0)}</h1>
+				<h2 className={styles.description}>{weatherInfo.weather[0].main}</h2>
+			</div>
+			<hr />
+			<div className={styles.extra}>
+				<div className={styles.extra__unit}>
+					<span>{weatherInfo.wind.speed}km/h</span>
+					<p>Wind</p>
+				</div>
+
+				<div className={styles.extra__unit}>
+					<span>{weatherInfo.main.humidity}%</span>
+					<p>Humidity</p>
+				</div>
+
+				<div className={styles.extra__unit}>
+					<span>{weatherInfo.main.pressure}</span>
+					<p>Pressure</p>
+				</div>
+			</div>
 		</div>
 	)
 }
