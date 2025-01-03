e16eec75e55b289664075b286cb0b905b9d561da
Kirill <razdor.2019@mail.ru>
2025-01-04 00:23:21 +0300

    feat: add emoji

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './styles/index.scss'
 import useWeatherFetch from './hooks/useWeatherFetch'
 import Header from './components/Header/Header'
@@ -7,10 +7,36 @@ import 'primeicons/primeicons.css'
 import CurrentWeather from './components/CurrentWeather/CurrentWeather'
 
 const App = () => {
+	const [emoji, setEmoji] = useState('/emoji/greeting.svg')
 	const [city, setCity] = useState<cityType>({ lat: '', lon: '', name: '' })
 	const [requestType, setRequestType] = useState('weather')
 	const { weatherInfo, loading, error } = useWeatherFetch({ city, requestType })
 	console.log(new Date(1735916400))
+
+	const changeEmoji = () => {
+		if (!weatherInfo || !weatherInfo.main || !weatherInfo.main.temp) {
+			setEmoji('/emoji/greeting.svg')
+			return
+		}
+		const x = weatherInfo.main.temp
+		if (x <= -10) {
+			setEmoji('/emoji/cold.svg')
+		} else if (x <= 0) {
+			setEmoji('/emoji/zero.svg')
+		} else if (x <= 10) {
+			setEmoji('/emoji/normal.svg')
+		} else if (x <= 20) {
+			setEmoji('/emoji/warm.svg')
+		} else if (x > 20) {
+			setEmoji('/emoji/hot.svg')
+		} else {
+			setEmoji('/emoji/greeting.svg')
+		}
+	}
+	useEffect(() => {
+		changeEmoji()
+	}, [weatherInfo])
+
 	return (
 		<div className='app'>
 			<div className='main-section'>
@@ -27,6 +53,11 @@ const App = () => {
 					</>
 				)}
 			</div>
+			{requestType === 'weather' && (
+				<div className='emoji-section'>
+					<img src={emoji} alt='Hi there!' />
+				</div>
+			)}
 		</div>
 	)
 }
